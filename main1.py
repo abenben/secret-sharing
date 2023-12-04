ee023923499b3d121fe2f80c4c5bd923387f6cf2
abenben <abenbenben@gmail.com>
2023-12-05 08:29:25 +0900

    更新

--- main1.py
+++ main1.py
@@ -2,7 +2,11 @@ import tss
 
 print('[開始]')
 
-with open('./input/memo.txt', 'r') as file:
+filename = 'memo.txt'
+#filename = 'report1.pdf'
+
+# バイナリモードでPDFファイルを読み込む
+with open(f'./input/' + filename, 'rb') as file:
     secret_data = file.read()
 
 shares = tss.share_secret(2, 2, secret_data, 'secretid42', tss.Hash.SHA256)
@@ -19,7 +23,7 @@ for i in range(2):
 # 秘密を回復
 try:
     recovered_secret = tss.reconstruct_secret(loaded_shares)
-    with open(f'./output/memo.txt', 'wb') as file:
+    with open(f'./output/' + filename, 'wb') as file:
         file.write(recovered_secret)
     print('[正常終了]')
 except tss.TSSError:
