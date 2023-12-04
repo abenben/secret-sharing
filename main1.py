acb91f0ac53d4dd7e7d0c6c9fc3d842186c8a283
abenben <abenbenben@gmail.com>
2023-12-05 08:14:34 +0900

    更新

--- main1.py
+++ main1.py
@@ -1,27 +1,26 @@
 import tss
 
-# 秘密をファイルから読み込む
+print('[開始]')
+
 with open('./input/memo.txt', 'r') as file:
     secret_data = file.read()
 
-# 秘密を分割
 shares = tss.share_secret(2, 2, secret_data, 'secretid42', tss.Hash.SHA256)
 
-# 分割された秘密を個別のファイルに保存（バイナリモードで開く）
 for i, share in enumerate(shares):
-    with open(f'./middle/share_{i}.bin', 'wb') as file:  # 'wb' を使用してバイナリモードで開く
+    with open(f'./middle/share_{i}.bin', 'wb') as file:
         file.write(share)
 
-# 分割された秘密をファイルから読み込む（バイナリモードで読み込む）
 loaded_shares = []
-for i in range(2):  # 2つの分割ファイルを読み込む
-    with open(f'./middle/share_{i}.bin', 'rb') as file:  # 'rb' を使用してバイナリモードで読み込む
+for i in range(2):
+    with open(f'./middle/share_{i}.bin', 'rb') as file:
         loaded_shares.append(file.read())
 
 # 秘密を回復
 try:
     recovered_secret = tss.reconstruct_secret(loaded_shares)
-    with open(f'./output/memo.txt', 'wb') as file:  # 'wb' を使用してバイナリモードで開く
+    with open(f'./output/memo.txt', 'wb') as file:
         file.write(recovered_secret)
+    print('[正常終了]')
 except tss.TSSError:
-    pass  # エラー処理
+    print('秘密の回復に失敗しました')
